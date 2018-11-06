af7f26a14505ff72da2aec2957549bdfcf3d7d7a
jmpark6846 <jmpark6846@github.com>
2018-11-06 09:51:33 +0900

    account app 추가

--- budget/forms.py
+++ budget/forms.py
@@ -5,3 +5,15 @@ class BudgetForm(forms.ModelForm):
     class Meta:
         model = Budget
         fields = ['name', 'amount']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'autocomplete': 'off',
+                'placeholder': '예산 항목',
+                'class': 'form-control',
+            }),
+            'amount': forms.NumberInput(attrs={
+                'autocomplete': 'off',
+                'placeholder': '금액',
+                'class': 'form-control',
+            })
+        }
