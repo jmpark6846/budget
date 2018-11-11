f310099be92be560eea5130a82b470106b230ec6
jmpark6846 <jmpark6846@gmail.com>
2018-11-11 14:29:45 +0900

    superuser command 수정

--- budget/management/commands/createsu.py
+++ budget/management/commands/createsu.py
@@ -5,9 +5,6 @@ import os
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        ADMIN_NAME = os.environ['ADMIN_NAME']
-        ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
-
-        if not User.objects.filter(username=ADMIN_NAME).exists():
-            User.objects.create_superuser(ADMIN_NAME, "admin@admin.com", ADMIN_PASSWORD)
+        if not User.objects.filter(username=os.environ['ADMIN_NAME']).exists():
+            User.objects.create_superuser(os.environ['ADMIN_NAME'], "admin@admin.com", os.environ['ADMIN_PASSWORD'])
             self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
