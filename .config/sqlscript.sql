42c9d452b527261af566c38d45f228fd90d1f242
jmpark6846 <jmpark6846@gmail.com>
2018-11-12 08:55:38 +0900

    DB 설정 mysql file로  변경

---
+++ .config/sqlscript.sql
@@ -0,0 +1,2 @@
+alter database ebdb character set utf8mb4 collate utf8mb4_unicode_ci;
+alter table ebdb.budget_budget convert to character set utf8mb4 collate utf8mb4_unicode_ci;
--- proj/settings/prod.py
+++ proj/settings/prod.py
@@ -10,13 +10,8 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'OPTIONS':{
+                'read_default_file':'.config/my.cnf'
+            }
         }
     }
-
-
-
