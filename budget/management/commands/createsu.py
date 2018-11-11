83d3131c70031f15761e8227a067a4e9a823be12
jmpark6846 <jmpark6846@gmail.com>
2018-11-11 14:27:32 +0900

    superuser 추가

--- budget/management/commands/createsu.py
+++ budget/management/commands/createsu.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
+import os
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+        ADMIN_NAME = os.environ['ADMIN_NAME']
+        ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
+
+        if not User.objects.filter(username=ADMIN_NAME).exists():
+            User.objects.create_superuser(ADMIN_NAME, "admin@admin.com", ADMIN_PASSWORD)
             self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
