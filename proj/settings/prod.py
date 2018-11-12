c3cf8b34e7d5e69fe298d7e98a1c384c81c50656
jmpark6846 <jmpark6846@gmail.com>
2018-11-12 09:51:45 +0900

    DB 설정 되돌리기

--- proj/settings/prod.py
+++ proj/settings/prod.py
@@ -10,8 +10,16 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'OPTIONS':{
-                'read_default_file': os.path.join(BASE_DIR,'.config/my.cnf')
-            }
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+            # 'OPTIONS':{
+            #     'read_default_file': os.path.join(BASE_DIR,'.config/my.cnf')
+            # }
         }
     }
+
+
+
